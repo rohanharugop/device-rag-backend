5f931cd327a5808b438786b7c8700579a95276a1
Rohan Harugop <rohan.harugop@gmail.com>
2026-04-10 19:41:58 +0530

    fixed CORS issues

--- app/main.py
+++ app/main.py
@@ -2,14 +2,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-print("OPENAI KEY:", os.getenv("OPENAI_API_KEY"))
+# print("OPENAI KEY:", os.getenv("OPENAI_API_KEY"))
+
 
 from fastapi import FastAPI
 from app.api.routes.device import router as device_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Device RAG API",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # or ["*"] for quick testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # ✅ THIS LINE WAS MISSING
