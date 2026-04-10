896377da2c6b0f28cc30a5439a20379999cccad8
Rohan Harugop <rohan.harugop@gmail.com>
2026-04-10 09:59:54 +0530

    safety test

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from dotenv import load_dotenv
-load_dotenv()   # ✅ LOAD FIRST (TOP OF FILE)
+# from dotenv import load_dotenv
+# load_dotenv()   # ✅ LOAD FIRST (TOP OF FILE)
+
+# from fastapi import FastAPI
+# from app.api.routes.device import router as device_router
+
+# app = FastAPI()
+
+# app.include_router(device_router)
+
+
+
+
 
 from fastapi import FastAPI
-from app.api.routes.device import router as device_router
 
 app = FastAPI()
 
-app.include_router(device_router)
+@app.get("/")
+def root():
+    return {"status": "running"}
